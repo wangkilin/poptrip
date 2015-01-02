7f1e8e2fbf9505c94b153f0296b3dccb4521bd6f
wangkilin <wangkilin@126.com>
2015-01-02 20:02:38 +0800

    display irc information when media is being played

--- Apps/PopNic/Tpl/Test/Index/Playlist.php
+++ Apps/PopNic/Tpl/Test/Index/Playlist.php
@@ -2,15 +2,52 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<!-- Website Design By: www.happyworm.com -->
 <title>Demo : The jPlayerPlaylist Object</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style>
+#lrcInfoList {
+    height : 100px;
+    overflow : hidden;
+}
+#lrcListZone
+{
+}
+#lrcListZone > li {
+    list-style: outside none none;
+    margin-left: -30px;
+}
+.lrcCurrentPlaying {
+    background-color: #ffffff;
+}
+</style>
 <link href="__PUBLIC__/js/jPlayer-2.9.0/skin/blue.monday/jplayer.blue.monday.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="__PUBLIC__/js/jPlayer-2.9.0/lib/jquery.min.js"></script>
 <script type="text/javascript" src="__PUBLIC__/js/jPlayer-2.9.0/dist/jplayer/jquery.jplayer.js"></script>
 <script type="text/javascript" src="__PUBLIC__/js/jPlayer-2.9.0/dist/add-on/jplayer.playlist.js"></script>
 <script type="text/javascript">
 //<![CDATA[
+/**
+ * 将时间戳格式化成实数
+ */
+function timeToDouble (timeString)
+{
+	var timeInfo = timeString.toString().split('.');
+	if (timeInfo.length==1) {
+		timeInfo.push('00');
+	}
+	var baseTimeInfo = timeInfo[0].split(':');
+	// 推送成 小时,分， 秒 形式
+	while (baseTimeInfo.length < 3) {
+		baseTimeInfo.unshift('0');
+	}
+
+	var doubleString = (parseInt(baseTimeInfo[0])*3600
+	                 + parseInt(baseTimeInfo[1])*60
+	                 + parseInt(baseTimeInfo[2]))
+	                 + '.' + timeInfo[1];
+
+    return doubleString;
+}
 /**
  * 载入LRC
  */
@@ -20,7 +57,24 @@ function loadLrc (playlistInstance, index)
 		//window.console.debug(playlistInstance.playlist[index].id);
 		var params = {id:index};
 		$.getJSON('LoadLrcById.html', params, function (response) {
-			$('#lrcInfoList').html(response);
+			var lrcList = response.split("\n");
+			var liHtml = '';
+			var reg = /^\[(:|\d|\.)+\]/g;
+			var liTime = '';
+			var timestamp = '';
+			for(var i=0; i<lrcList.length; i++) {
+				liTime = '';
+
+				if (reg.test(lrcList[i])) {
+					timestamp = lrcList[i].match(reg).toString();
+					lrcList[i] = lrcList[i].substr(timestamp.length);
+					timestamp = timestamp.substring(1, timestamp.length-1);
+					liTime = timeToDouble (timestamp);
+				}
+
+				liHtml = liHtml + '<li time="'+liTime+'">' + lrcList[i]+'</li>';
+			}
+			$('#lrcListZone').html(liHtml);
 		});
 	}
 }
@@ -31,12 +85,73 @@ function playtimeChangeTrigger (totalTime, leftTime)
 {
     moveLrc(totalTime - leftTime);
 }
+
+/**
+ * 载入LRC列表size
+ */
+function loadLrcZoneSizeInfo (lrcZoneId)
+{
+	if (typeof globalLrcZoneSizeInfo !== 'undefined') {
+		return globalLrcZoneSizeInfo;
+	}
+
+	var lrcZone = $('#'+lrcZoneId);
+	globalLrcZoneSizeInfo = {height: 0, width: 0, left: 0, top:0};
+	if (lrcZone.length) {
+		globalLrcZoneSizeInfo.height = lrcZone.height();
+		globalLrcZoneSizeInfo.width = lrcZone.width();
+		globalLrcZoneSizeInfo.left = lrcZone.offset().left;
+		globalLrcZoneSizeInfo.top = lrcZone.offset().top;
+	}
+
+	return globalLrcZoneSizeInfo;
+}
 /**
  * 移动LRC内容
  */
 function moveLrc (playingTimePoint)
 {
-    window.console.debug(playingTimePoint);
+	if (typeof globalSettingCurrentPlaying === 'undefined') {
+		globalSettingCurrentPlaying = false;
+	}
+	if (globalSettingCurrentPlaying) {
+		return;
+	}
+	globalSettingCurrentPlaying = true;
+    //window.console.debug(playingTimePoint);
+    var $liList = $('#lrcListZone li');
+    var liTime = 0; // set the time point of li item
+    var lrcInfoSize = loadLrcZoneSizeInfo ('lrcInfoList'); // load lrc zone width and height
+    var $currentLi;
+    var offset;
+    var marginTopOfLrcListZone = parseInt($('#lrcListZone').css('margin-top'));
+    var newMarginTop = parseInt(lrcInfoSize.height/2);
+
+    // 设置当前播放的语句
+    for(var i=$liList.length-1; i>=0; i--) {
+    	$currentLi = $liList.eq(i);
+    	liTime = $currentLi.attr('time');
+    	if (liTime=='') {
+        	continue;
+    	}
+        if (liTime<=playingTimePoint) {
+            if ($currentLi.hasClass('lrcCurrentPlaying')) {
+                console.debug('li is set');
+                break;
+            }
+        	$currentLi.addClass('lrcCurrentPlaying');
+        	$currentLi.siblings().removeClass('lrcCurrentPlaying');
+        	newMarginTop = marginTopOfLrcListZone
+        	           + parseInt( lrcInfoSize.top
+                	             - $currentLi.offset().top
+                	             + lrcInfoSize.height/2
+                	     )
+                	   + 'px';
+        	$('#lrcListZone').css('margin-top', newMarginTop);
+            break;
+        }
+    }
+    globalSettingCurrentPlaying = false;
 }
 
 $(document).ready(function(){
@@ -178,6 +293,7 @@ $(document).ready(function(){
 		</div>
 	</div>
 	<div id="lrcInfoList" class="jp-video-270p">
+	  <ul id="lrcListZone"></ul>
 	</div>
 </div>
 			<p style="margin-top:1em;">
