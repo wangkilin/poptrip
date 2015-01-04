a596e96faa27bb483bb118a4f975b82f76b8204c
wangkilin <wangkilin@126.com>
2015-01-04 20:55:08 +0800

    ebook

---
+++ Apps/PopNic/Lib/Model/Ebook.class.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * book list
+ * chapter
+ *
+ *
+ * @author Mingxia
+ *
+ */
+class EbookModel extends Model
+{
+    public function getBookList()
+    {
+
+    }
+
+    public function addBook() {}
+
+    public function updateBook () {}
+
+    public function deleteBook () {}
+    public function  addComment () {}
+    public function updateComment () {}
+    public function deleteComment () {}
+    public  function addPage () {}
+    public function updatePage () {}
+    public function addChapter () {}
+    public function addNextPage () {}
+    public function addPrevPage () {}
+    public function thumbsUpComment () {}
+
+    public function getChapterByBookId ($bookId)
+    {
+
+    }
+
+    public function getPageByShortTag ($shortTag, $bookId)
+    {
+
+    }
+
+    public  function getCommentsByPageId ($pageId)
+    {
+
+    }
+
+
+}
