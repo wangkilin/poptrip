5d9322ce177c1945abe24ace79ad2242c50f6d42
wangkilin <wangkilin@126.com>
2015-01-05 08:21:57 +0800

    ebook css

---
+++ Apps/PopNic/Tpl/Ebook/Index/Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Demo : The jPlayerPlaylist Object</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style>
+#lrcInfoList {
+    height : 100px;
+    overflow : hidden;
+}
+#lrcListZone
+{
+}
+#lrcListZone > li {
+    list-style: outside none none;
+    margin-left: -30px;
+}
+.lrcCurrentPlaying {
+    background-color: #ffffff;
+}
+</style>
+<link href="__PUBLIC__/css/Ebook/ebook.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="__PUBLIC__/js/jquery.1.8.2.js"></script>
